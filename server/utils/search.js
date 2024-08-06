28cd7ea3347e29a8fb8770fcc179444ec59062a7
bharat singh <bharat.singh@devtechnosys.info>
2024-08-06 18:20:52 +0530

    code run

--- server/utils/search.js
+++ server/utils/search.js
@@ -1,11 +1,6 @@
 const SerpApi = require("google-search-results-nodejs");
 const search = new SerpApi.GoogleSearch("c81fe9367a4618cd74917cd0614b11085cb155e485946533da71b2068d376216");
 
-/**
- * Extract names from SerpApi reverse image search results
- * @param {string} imageUrl - The URL of the image to search
- * @returns {Promise<Array<string>>} - Array of names found in the search results
- */
 const getNamesFromImage = async (imageUrl) => {
     try {
         console.log("Cloudinary URL", imageUrl)
@@ -26,9 +21,6 @@ const getNamesFromImage = async (imageUrl) => {
 
         const json = await getJson();
         organicResults.push(...json.image_results);
-
-
-        console.log("json", JSON.stringify(organicResults))
         const names = organicResults
             .map(result => result.snippet_highlighted_words)
             .flat()
